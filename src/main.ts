1874a3b7d84d878a46a1c0afeb71593f4b9485cc
guillaume <guillaume.kerherve@scalian.com>
2023-06-06 21:52:25 +0200

    feat/ajout-scene : ajout de la scene phaser

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-import './style.css'
+import 'phaser';
 
+class MyScene extends Phaser.Scene {
+  
+
+  constructor() {
+    super('my-scene');
+  }
+
+  preload() {
+    
+  }
+
+  create() {
+
+  }
+
+  update() {
+    
+  }
+}
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 1500,
+  height: 1000,
+  scene: MyScene
+};
+
+const game = new Phaser.Game(config);
