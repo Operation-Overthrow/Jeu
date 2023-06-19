aa18dd965a0327b0e5fa808b1a78c7860962b9bf
guillaume <guillaume.kerherve@scalian.com>
2023-06-19 13:59:48 +0200

    ajout du noyau lors de l'initialisation de la scène

---
+++ src/models/Core.ts
@@ -0,0 +1,13 @@
+export class Core {
+    private hp: number;
+    private x: number;
+    private y: number;
+    
+    constructor(hp:number,x:number,y:number){
+        this.hp = hp;
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
